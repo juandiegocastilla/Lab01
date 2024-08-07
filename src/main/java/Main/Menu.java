9300e8686266d5af870639301e075601fc042580
juandiegocastilla <est.juan.castilla@unimilitar.edu.co>
2024-08-07 12:38:43 -0500

    Configuracion para digitar numeros en tablero

--- src/main/java/Main/Menu.java
+++ src/main/java/Main/Menu.java
@@ -16,14 +16,14 @@ public class Menu {
         String titulo = "Lab01: Introducción al lenguaje Java";
 
         // *** AJUSTAR LOS MENSAJES QUE HAN DE APARECER EN EL MENU ***
-        String opciones[] = {"Calculadora consola", "Calculadora gráfica (Swing)", "otra opcion"};
+        String opciones[] = {"Calculadora consola", "Calculadora gráfica"};
 
         final int numop = opciones.length;
 
         // Crear el marco de la ventana
         frame = new JFrame(titulo);
-        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
         frame.setSize(400, 50 * numop);
         frame.setLayout(new BorderLayout());
 
@@ -48,8 +48,7 @@ public class Menu {
                         case 1:
                             botones[index].setEnabled(!botones[index].isEnabled());
                             new CalculadoraGrafica().setVisible(true);
-                        case 2:
-                            System.out.println("Tercera opcion");
+                        
                     }
                     // *** FINAL AJUSTES ***
                 }
