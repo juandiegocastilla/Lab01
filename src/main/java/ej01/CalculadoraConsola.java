b6db94bfdcc06a43755e258c254d2956e5b8beec
juandiegocastilla <est.juan.castilla@unimilitar.edu.co>
2024-08-10 18:42:54 -0500

    Creaccion menu calculadora consola

--- src/main/java/ej01/CalculadoraConsola.java
+++ src/main/java/ej01/CalculadoraConsola.java
@@ -1,7 +1,23 @@
 package ej01;
-
+import java.util.Scanner;
 public class CalculadoraConsola {
     public static void main (String args[]) {
         System.out.println("Calculadora consola");
+        Scanner leer= new Scanner(System.in);
+        double conteo=0;
+        while(reset){
+            System.out.print("1.Sumar");
+            System.out.print("2.Restar");
+            System.out.print("3.Multiplicar");
+            System.out.print("4.Dividir");
+            System.out.print("5.Seno");
+            System.out.print("6.Coseno");
+            System.out.print("7.Tangente");
+            System.out.print("8.IVA");
+            
+            int option=leer.nextInt();
+            switch(option){
+            }
+        }
     }
 }
